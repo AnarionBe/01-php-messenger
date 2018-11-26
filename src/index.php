900d576cecdd63d077d5d4dbb941ff40f5e54eb2
olivierk95 <keutgensolivier@gmail.com>
2018-11-26 16:59:54 +0100

    style page d'accueil

--- src/index.php
+++ src/index.php
@@ -9,40 +9,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Meow</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"> 
 </head>
 
 
 <body>
     <div class="container">
 
-        <img class="card-img" src="/img/LionBackground.jpg" alt="Card image">
-
-        <!-- Header -->
-            <div class="card-img-overlay" >
-                <div class="col-sm-8">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            <a href= "index.php"><img src=/img/MeowLogo_BeCode.png alt="Logo Meow" class="logoMeow"/></a>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <?php if(sizeof($_SESSION) == 0) { ?>
-                            <a href="./pages/login.php" class="btn btn-primary">Se connecter</a>
-                            <?php } else echo "connecté !"; ?>
-                        </div>
-                    </div>
-                </div>
+        <!-- Carte de connexion -->
+        <div class="carte" >
+            <div class="carteImage">
+                <a href= "index.php"><img src=/img/MeowLogo_BeCode.png alt="Logo Meow" class="logoMeow"/></a>
             </div>
+            
+            <div class="carteTexte">
+                <h4>Bienvenue sur le chat Meow!</h4>
+                <p>Parce que le(s) chat(s) c'est la vie.</p>
+                <?php if(sizeof($_SESSION) == 0) { ?>
+                <a href="./pages/login.php" class="bouton">Se Meower</a>
+                <?php } else echo "connecté !"; ?>
+            </div>
+                
+        </div>
 
    
 
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
