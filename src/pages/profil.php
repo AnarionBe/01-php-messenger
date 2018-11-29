92cd450597a06e3794ec8fd1dc3d4ff5a35ca5fc
domreka <dominiquebosman02@gmail.com>
2018-11-29 16:39:03 +0100

    avancement profil

--- src/pages/profil.php
+++ src/pages/profil.php
@@ -1,107 +1,117 @@
 <?php
+  session_start();
+  require '../traitements/avatar.php';
+?> 
 
-?>
-
- <!DOCTYPE html>
- <html lang="fr" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <link rel="stylesheet" type="text/css" href="styleprofil.css" />
-     <title>Meowser - Modifier le profil</title>
-   </head>
-   <body>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styleprofil.css" />
+    <title>Meowser - Modifier le profil</title>
+  </head>
+  <body>
 
      <h1>Meowser</h1> <!--- ajout prénom a l'affichage --->
-     <h2>Modifier votre profil</h2>
+     <h2>Bienvenue, <?php echo $_POST['prenom'] ?></h2>
 
      <p>
        <div class="configProfile">
-         <form class="profileModif" action="" method="post">
+          <form action="../traitements/User_Profile.php" method="post" class="profileModif">
 
-           <div class="lname">
+            <div class="nom">
 
-             <div class="lnameLab">
-              <label for="lname">Nom</label>
-             </div>
+              <div class="nomLab">
+                <label for="nom">Nom :</label>
+              </div>
 
-             <div class="lnameInput">
-              <input type="text" name="lname" value="">
-             </div>
+              <div class="nomInput">
+                <input type="text" name="nom" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="fname">
+            <div class="prenom">
 
-             <div class="fnameLab">
-              <label for="fname">Prénom</label>
-             </div>
+              <div class="prenomLab">
+                <label for="prenom">Prénom :</label>
+              </div>
 
-             <div class="fnameInput">
-              <input type="text" name="fname" value="">
-             </div>
+              <div class="prenomInput">
+                <input type="text" name="prenom" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="email">
+            <div class="email">
 
-             <div class="emailLab">
-              <label for="email">Email</label>
-             </div>
+              <div class="emailLab">
+                <label for="email">Email :</label>
+              </div>
 
-             <div class="emailInput">
-              <input type="text" name="email" value="">
-             </div>
+              <div class="emailInput">
+                <input type="text" name="email" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="confirmEmail">
+            <div class="confirmEmail">
 
-             <div class="confirmEmailLab">
-              <label for="confirmemail">Confirmation de l'email</label>
-             </div>
+              <div class="confirmEmailLab">
+                <label for="confirmemail">Confirmation de l'email :</label>
+              </div>
 
-             <div class="confirmEmailInput">
-              <input type="text" name="confirmemail" value="">
-             </div>
+              <div class="confirmEmailInput">
+                <input type="text" name="confirmEmail" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="password">
+            <div class="password">
 
-             <div class="passwordLab">
-              <label for="password">Nouveau mot de passe</label>
-             </div>
+              <div class="passwordLab">
+                <label for="password">Nouveau mot de passe :</label>
+              </div>
 
-             <div class="passwordInput">
-              <input type="password" name="password" value="">
-             </div>
+              <div class="passwordInput">
+                <input type="password" name="password" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="confirmPassword">
+            <div class="confirmPassword">
 
-             <div class="confirmPasswordLab">
-              <label for="confirmpassword">Confirmation du mot de passe</label>
-             </div>
+              <div class="confirmPasswordLab">
+                <label for="confirmpassword">Confirmation du mot de passe :</label>
+              </div>
 
-             <div class="confirmPasswordInput">
-              <input type="password" name="confirmpassword" value="">
-             </div>
+              <div class="confirmPasswordInput">
+                <input type="password" name="confirmPassword" value="">
+              </div>
 
-           </div>
+            </div>
 
-           <div class="submit">
-             <button type="button" name="submitButton" value="Confirmer">Confirmer</button>
-           </div>
+           <!--< <div class="avatar">
+              <form action="avatar.php" method="post" enctype="multipart/formdata">
+                <input type="file" name="profilpic">
+                <input type="submit" name="send-avatar" value="Uploader votre avatar">
+              </form>
+            </div>-->
 
-           <div class="cancel">
-             <button type="reset" name="resetButton" value="Annuler">Annuler</button>
-           </div>
+            <div class="buttons">
+              <div class="submit">
+                <input type="submit" name="send-profile" value="Confirmer">
+              </div>
+
+              <div class="cancel">
+                <button type="reset" name="resetButton" value="Annuler">Annuler</button>
+              </div>
+            </div>
 
 
-         </form>
+          </form>
         </div>
-     </p>
+    </p>
 
-   </body>
- </html>
+  </body>
+</html>
--- src/traitements/User_Profile.php
+++ src/traitements/User_Profile.php
@@ -1,72 +1,72 @@
 <?php
-session_start();
+    session_start();
 
-    require '../class/user.php';
-    require '../pages/profil.php';
+    require(dirname(__DIR__) . '/class/User.php');
 
-    // Confirmation des modifications
-
-    // Changement de MDP
-
-    if(!empty($_POST['password']) || $_POST['password'] != $_POST['confirmPassword']){
-        $_SESSION['email'] = "Les mots de passes ne correspondent pas";
-    }else{
-
+    try {
+        $bdd = new PDO('mysql:host=mysql;dbname=messenger;charset=utf8', 'messenger', 'messenger');
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
     }
 
+    if(isset($_POST["send-profile"])){
+        // Changement de MDP
+        if (!empty($_POST)){
+            if(!empty($_POST['password']) || $_POST['password'] != $_POST['confirmPassword']){
+                echo("Les mots de passes ne correspondent pas.");
+            }else{
+                $email = $_SESSION['email']->id;
+                $password = setPassword($value);
+                $pdo->prepare('UPDATE users SET password = ? WHERE email = ?')->execute([$password, $email]);
+                echo("Votre mot de passe à bien été mis à jour.");
+            }
+        }
+    }
+    
+    if(isset($_POST["send-avatar"])){
+        echo("prout");
+    }
 
+    //MDP
 
-/*    // CHANGEMENT DE MOT DE PASSE
-    logged_only();
-    if (!empty($_POST)){
-        if (empty($_POST['password']) || $_POST['password'] != $_POST['confirmPassword']){ // Si le mdp est vide, ou si les mdp ne correspondent pas
-            $_SESSION[''] = "Les mots de passes ne correspondent pas"; // Alors ce message s'affichera sur la session de l'utilisateur
+   /* if (!empty($_POST)){
+        if(!empty($_POST['password']) || $_POST['password'] != $_POST['confirmPassword']){
+            echo("Les mots de passes ne correspondent pas");
         }else{
-            $email = $_SESSION['']-> id;
-            $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash du mdp
-            $pdo->prepare('UPDATE users SET password = ? WHERE id = ?')->execute([$password, $email]); // Procède à la mise à jour du mdp de l'utilisateur
-            $_SESSION[''] = "Votre mot de passe à bien été mis à jour"; // Ce message s'affiche si l'opération est réussie lors de la session de l'utilisateur
+            $email = $_SESSION['email']->id;
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $pdo->prepare('UPDATE users SET password = ? WHERE email = ?')->execute([$password, $email]);
+            echo("Votre mot de passe à bien été mis à jour.");
         }
     }
 
-    // CHANGEMENT DU PRENOM
-    logged_only();
+    // Changement du prenom
+
     if (!empty($_POST)){
-        if (empty($_POST['fname']){
-            $_SESSION[''] = "";
-        }else{
-            $email = $_SESSION['']-> id; // A REMPLIR ?
-            $firstname = ""; // A REMPLIR
-            $pdo->prepare('UPDATE users SET firstname = ? WHERE id = ?')->execute([$firstname, $email]);
-            $_SESSION[''] = "Vos données ont bien été mises à jour."; // A REMPLIR
-        }
+            $email = $_SESSION['email']->id;
+            $prenom = "";
+            $pdo->prepare('UPDATE users SET firstname = ? WHERE email = ?')->execute([$firstname, $email]);
+            echo("Votre mot de passe à bien été mis à jour.");
     }
 
-    // CHANGEMENT DU NOM
-    logged_only();
+    // Changement du nom
+
     if (!empty($_POST)){
-        if (empty($_POST['lastname']){ // A MODIFIER
-           // $_SESSION[''] = ""; // A REMPLIR
-        }else{
-            $email = $_SESSION['']-> id; // A REMPLIR ?
-            $lastname = ""; // A REMPLIR
-            $pdo->prepare('UPDATE users SET lastname = ? WHERE id = ?')->execute([$lastname, $user_id]);
-            $_SESSION[''] = "Vos données ont bien été mises à jour."; // A REMPLIR
-        }
+        $email = $_SESSION['email']->id;
+        $nom = "";
+        $pdo->prepare('UPDATE users SET lastname = ? WHERE email = ?')->execute([$lastname, $email]);
+        echo("Vos données ont bien été mises à jour.");
     }
 
-    // CHANGEMENT DU MAIL
-    logged_only();
-    if (!empty($_POST)){
-        if ($_POST['email'] != $_POST['email2']){
-            $_SESSION['email'] = "Vos emails ne correspondent pas.";
-        }else{
-            $email = $_SESSION['email']-> id; // A REMPLIR
-            $email = ""; // A REMPLIR
-            $pdo->prepare('UPDATE users SET email = ? WHERE id = ?')->execute([$email, $user_id]);
-            $_SESSION[''] = "Vos données ont bien été mises à jour."; // A REMPLIR
-        }
-    }
-?>
+    // Changement du mail
 
-*/
+    if (!empty($_POST)){
+        if ($_POST['email'] != $_Post['confirmEmail']){
+            echo("Vos emails ne correspondent pas");
+        }else{
+            $email = $_SESSION['email']->id;
+            $email = "";
+            $pdo->prepare('UPDATE users SET email = ? WHERE email = ?')->execute([$email, $email]);
+            echo("Votre email à bien été mis à jour.");
+        }
+    } */
