2715dcfef181ac192c0e3368fd2af6ebbed1cd8e
olivierk95 <keutgensolivier@gmail.com>
2018-11-30 11:50:25 +0100

    fonction emojis dans les messages

--- src/pages/chat.php
+++ src/pages/chat.php
@@ -13,31 +13,64 @@
     <link rel="stylesheet" href="chat-style.css">
 </head>
 
-<body>
-    <form action="../traitements/createConv.php" method="post">
-        <input type="text" name="title" id="title"><label for="title">Titre</label>
-        <br><input type="submit">
+
+
+<?php
+
+try
+{
+	$add_message = new PDO('mysql:host=mysql;dbname=messenger;charset=utf8', 'messenger', 'messenger');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+// Récupération des 10 derniers messages
+$answer = $add_message->query('SELECT message FROM messages ORDER BY id DESC LIMIT 0, 10');
+
+// Affichage de chaque message
+while ($donnees = $answer->fetch())
+{ ?>
+    <form method="post" action="./traitements/traitementMessageCreate.php">
+        <textarea name="modification" type="text"><?php echo ($donnees['message']); ?></textarea>
+        <input type="submit" value="Modifier">
+        <input type="text" name="id" value=1>
     </form>
+<?php }
 
-    <form action="../traitements/insertReaction.php" method="post">
+$answer->closeCursor();
 
-<br>
-<br>
-<br>
-<br>
+?>
+ 
+
+
+<body>
+
+    <p>
+        <form method="post" action="./traitements/traitementMessageCreate.php">
+            <textarea name = "message" id="message">
+            Votre message ici
+            </textarea>
+            <input type="submit" value="Envoyer mon message" />
+        </form>
+    <p>
+
+    <!--<form action="../traitements/insertReaction.php" method="post"> -->
+
 <!-- Code qui permet de faire le Popup-->
     <div class="popup" onclick="popupFunction()">
         <img src="../img/cat_emojis.png" alt="sélectionner un émojis"  title="Ajoutez une réaction"/>
         <span class="popupContent" id="myPopup">
-            <img class="emojis" src="../img/smilies/cat_smile.png">
-            <img class="emojis" src="../img/smilies/cat_laugh.png">
-            <img class="emojis" src="../img/smilies/cat_lol.png">
-            <img class="emojis" src="../img/smilies/cat_love.png">
-            <img class="emojis" src="../img/smilies/cat_sad.png">
-            <img class="emojis" src="../img/smilies/cat_surprised.png">
-            <img class="emojis" src="../img/smilies/cat_kiss.png">
-            <img class="emojis" src="../img/smilies/cat_angry.png">
-            <img class="emojis" src="../img/smilies/cat_determined.png">
+            <img class="emojis" src="../img/smilies/cat_smile.png" onclick="emojiFunction()" id="emoji1" />
+            <img class="emojis" src="../img/smilies/cat_laugh.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_lol.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_love.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_sad.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_surprised.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_kiss.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_angry.png" onclick="emojiFunction()" />
+            <img class="emojis" src="../img/smilies/cat_determined.png" onclick="emojiFunction()" />
         </span>
     </div>
 
@@ -47,6 +80,20 @@ classList.toggle permet de connecter la fonction html au css -->
         function popupFunction() {
             var popup = document.getElementById("myPopup");
             popup.classList.toggle("show");
+        }
+    </script>
+
+<!-- Fonction qui permet d'afficher les emojis dans le texte lorsqu'ils sont sélectionnés -->
+    <script>
+        function emojiFunction() {
+            var emoji = document.getElementsByClassName("emojis");
+            var emojiInMessage = document.getElementById("message").innerHTML;
+            var image = document.images.emoji1;
+
+            
+            document.getElementById("message").innerHTML = image;
+            
+            
         }
     </script>
 
